1c9ce17d1a7f5bed80d1955e44bf8d7fe8d2e6e6
Ward Bell <wardbell@hotmail.com>
2014-09-22 12:56:36 -0700

    Fix zFloat to work with Ng v.1.3.0-rc Still works with 1.2 See http://stackoverflow.com/questions/25895349/zfloat-directive-for-angular-no-work/25981014#25981014 and https://github.com/angular/angular.js/issues/9218

--- breeze.directives.js
+++ breeze.directives.js
@@ -1,7 +1,7 @@
 ﻿/*
  * Breeze Labs: Breeze Directives for Angular Apps
  *
- *  v.1.3.8
+ *  v.1.3.9
  *
  *  Usage:
  *     Make this module a dependency of your app module:
@@ -53,7 +53,7 @@
                 
                 function equivalenceFormatter(value){
                    var viewValue = ngModelCtrl.$viewValue // could have used 'elm.val()'
-                   return (value === +viewValue) ? viewValue : value;
+                   return (+value === +viewValue) ? viewValue : value;
                 }
             }
         };
